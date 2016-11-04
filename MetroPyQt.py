85acac040067de312fa07992a0f6832f64aeb263
Bigsabi <az999897@hotmail.com>
2016-11-05 00:13:11 +0800

    构建部分界面

--- MetroPyQt.py
+++ MetroPyQt.py
@@ -1,10 +1,63 @@
-#coding:utf-8
-import sys
-from PyQt4 import QtGui
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
  
-app = QtGui.QApplication(sys.argv)
-widget = QtGui.QWidget()
-widget.resize(250, 150)
-widget.setWindowTitle('PyQt')
-widget.show()
-sys.exit(app.exec_())
+#system imports
+import sys
+ 
+#pyqt imports
+from PyQt4 import QtCore,QtGui
+from PyQt4.QtCore import Qt
+
+class MainWindow(QtGui.QWidget):
+    def __init__(self):
+ 
+        QtGui.QWidget.__init__(self)
+ 
+        #初始化position
+        self.m_DragPosition=self.pos()
+ 
+        self.resize(300,150)
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+        self.setMouseTracking(True)
+        self.setStyleSheet("background-color:#5C4978;")
+ 
+        qwgt_Panel = QtGui.QWidget(self)
+        qwgt_Panel.setGeometry(QtCore.QRect(0,25,300, 125))
+        qwgt_Panel.setStyleSheet("QWidget{background-color:#3B475E;}")
+        #按钮一
+        qbtn_Update=QtGui.QPushButton(u"更新|修复",qwgt_Panel)
+        qbtn_Update.setGeometry(200,30,80,30)
+        qbtn_Update.setStyleSheet("QPushButton{background-color:#16A085;border:1px solid;color:#ffffff;font-size:15px;}""QPushButton:hover{background-color:#333333;}")
+        qbtn_Update.clicked.connect(self.showDialog)
+        qbtn_Close=QtGui.QPushButton(u"×",self)
+        qbtn_Close.setGeometry(275,0,25,25)
+        qbtn_Close.setStyleSheet("QPushButton{background-color:#5C4978;border:none;color:#F50505;font-size:15px;}""QPushButton:hover{background-color:#333333;}")
+
+        qlbl_Title = QtGui.QLabel(u"更新器",self)
+        qlbl_Title.setGeometry(125,0,125,25)
+        qlbl_Title.setStyleSheet("QLabel{color:#FF8C00;}")
+        #注册事件
+        self.connect(qbtn_Close,QtCore.SIGNAL("clicked()"),QtGui.qApp,QtCore.SLOT("quit()"))
+    def showDialog(self):
+        print "AAA"
+    #支持窗口拖动,重写两个方法
+    def mousePressEvent(self, event):
+        if event.button()==Qt.LeftButton:
+            self.m_drag=True
+            self.m_DragPosition=event.globalPos()-self.pos()
+            event.accept()
+ 
+    def mouseMoveEvent(self, QMouseEvent):
+        if QMouseEvent.buttons() and Qt.LeftButton:
+            self.move(QMouseEvent.globalPos()-self.m_DragPosition)
+            QMouseEvent.accept()
+ 
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.m_drag=False
+
+if __name__=="__main__":
+ 
+    mapp=QtGui.QApplication(sys.argv)
+    mw=MainWindow()
+    mw.show()
+    sys.exit(mapp.exec_())
---
+++ codeFile.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Template.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.resize(400, 300)
+        self.frame = QtGui.QFrame(Form)
+        self.frame.setGeometry(QtCore.QRect(60, 150, 341, 151))
+        self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtGui.QFrame.Raised)
+        self.frame.setObjectName(_fromUtf8("frame"))
+        self.label = QtGui.QLabel(self.frame)
+        self.label.setGeometry(QtCore.QRect(40, 40, 261, 21))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.widget = QtGui.QWidget(Form)
+        self.widget.setGeometry(QtCore.QRect(110, 30, 120, 80))
+        self.widget.setObjectName(_fromUtf8("widget"))
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(_translate("Form", "Form", None))
+        self.label.setText(_translate("Form", "TextLabel", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    Form = QtGui.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
