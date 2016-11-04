05c5c611088901412796c410b0eb4279ac6a5462
Bigcraft <az999897@hotmail.com>
2016-11-04 17:55:04 +0800

    给我保存！

--- main.py
+++ main.py
@@ -13,33 +13,35 @@ import Updater
 global UpdaterConfig
 global ServerMods
 global LocalMods
-configFile = open('./Config.json','r')
-#读取Config文件
-UpdaterConfig = json.load(configFile,object_hook=Updater.ConvertUpdaterHook)
-configFile.close()
-UpdaterConfig.LoadServerInfo()
-UpdaterConfig.ServerImage()
-req = urllib2.Request(UpdaterConfig.FilesAddress)
-res_data = urllib2.urlopen(req)
-res = res_data.read()
+global root
+def InitUpdater():
+    configFile = open('./Config.json','r')
+    #读取Config文件
+    UpdaterConfig = json.load(configFile,object_hook=Updater.ConvertUpdaterHook)
+    configFile.close()
+    UpdaterConfig.LoadServerInfo()
+    UpdaterConfig.ServerImage()
+    req = urllib2.Request(UpdaterConfig.FilesAddress)
+    res_data = urllib2.urlopen(req)
+    res = res_data.read()
 
-ServerMods = []
-LocalMods = []
-for jsonobject in json.loads(res,object_hook=Mod.ConvertModHook):
-    ServerMods.append(jsonobject)
-Helper.appendModInfo(LocalMods)
+    ServerMods = []
+    LocalMods = []
+    for jsonobject in json.loads(res,object_hook=Mod.ConvertModHook):
+        ServerMods.append(jsonobject)
+    Helper.appendModInfo(LocalMods)
+    
+    root.title(UpdaterConfig.Name)
+    totalLab = Label(root, text= "本地Mods:" +bytes(len(LocalMods)))
+    totalLab.pack()
+    serverLab = Label(root, text= "服务器Mods:" +bytes(len(ServerMods)))
+    serverLab.pack()
+    root.iconbitmap('./resources/Server.ico')
 
-root = Tk()
-root.geometry('320x160')
-root.title(UpdaterConfig.Name)
-totalLab = Label(root, text= "本地Mods:" +bytes(len(LocalMods)))
-totalLab.pack()
-serverLab = Label(root, text= "服务器Mods:" +bytes(len(ServerMods)))
-serverLab.pack()
-#w = Label(root, text="Hello, world!")
-#w.pack()
-root.iconbitmap('./resources/Server.ico')
-root.mainloop()
-
-print LocalMods
+if __name__ == "__main__":
+    root = Tk()
+    root.geometry('320x160')
+    InitUpdater()
+    root.mainloop()
+    #print LocalMods
 #print a
